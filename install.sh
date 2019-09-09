f367c56cd046d993d0c74c8c4275f7e96997a872
dru18 <46237773+dru18@users.noreply.github.com[1>
2019-09-09 13:41:54 +0530

    Signed-off-by: dru18 <46237773+dru18@users.noreply.github.com[1>
    	modified:   install.sh
    	modified:   uninstall.sh

--- install.sh
+++ install.sh
@@ -2,20 +2,22 @@
 
 # Uninstall ctree first.
 sudo bash uninstall.sh
+
+echo -e "[*] Installing dependencies...\n"
 # Install dependencies.
 sudo apt install tree cowsay toilet
+echo -e "[+] Dependencies uninstalled.\n"
 
+echo -e "[*] Installing ctree...\n"
 # Make 'ctree.sh' executable.
-sudo chmod 755 ctree.sh
+sudo chmod -v 755 ctree.sh
 
 # Copy 'ctree.sh' to /usr/bin/ directory.
-sudo cp ctree.sh /usr/local/bin/
+sudo cp -v ctree.sh /usr/local/bin/
 
 # Make soft link for /usr/bin/ctree.sh to /bin/ctree
-sudo ln -s /usr/local/bin/ctree.sh /bin/ctree
-
-# Make ctree directory in your Home directory.
-sudo mkdir $HOME/ctree
+sudo ln -vs /usr/local/bin/ctree.sh /bin/ctree
 
 # Copy About folder to your Home directory.
-sudo cp -r About/ $HOME/ctree
+sudo cp -vr About/ $HOME/.ctree
+echo -e "[+] ctree installed successfully."
--- uninstall.sh
+++ uninstall.sh
@@ -1,16 +1,18 @@
 #! /bin/bash
 
+echo -e "[*] Uninstalling ctree...\n"
 # Remove soft link 'ctree' from /bin/ directory.
-sudo rm /bin/ctree
+sudo rm -v /bin/ctree
 
 # Remove 'ctree.sh' from /usr/bin/ directory.
-sudo rm /usr/local/bin/ctree.sh
+sudo rm -v /usr/local/bin/ctree.sh
 
 # Remove ctree/ directory from your Home directory.
-sudo rm -r $HOME/ctree/
+sudo rm -vr $HOME/ctree/
 
 # purge dependencies.
 sudo apt purge tree cowsay toilet
 
 # autoremove remaining files of dependencies.
 sudo apt autoremove
+echo -e "\n[+] ctree uninstalled successfully."
